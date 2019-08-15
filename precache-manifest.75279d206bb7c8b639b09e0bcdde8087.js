707ca67133ae61247be9187300ea6566942dbbce
eddielewis-thinhle <eddie.thinhle@outlook.com>
2019-08-15 15:26:50 +0700

    Updates

---
+++ precache-manifest.75279d206bb7c8b639b09e0bcdde8087.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76fbd6c47a00e9e5524f4fe81b71baf1",
+    "url": "/reactjs-robotfriends/index.html"
+  },
+  {
+    "revision": "950a190373d22055143b",
+    "url": "/reactjs-robotfriends/static/css/main.77d39244.chunk.css"
+  },
+  {
+    "revision": "342d9bc222b993a147ee",
+    "url": "/reactjs-robotfriends/static/js/2.05fd20f3.chunk.js"
+  },
+  {
+    "revision": "950a190373d22055143b",
+    "url": "/reactjs-robotfriends/static/js/main.bf790813.chunk.js"
+  },
+  {
+    "revision": "75dd202702b5979017a8",
+    "url": "/reactjs-robotfriends/static/js/runtime~main.f86aa7d7.js"
+  }
+]);
