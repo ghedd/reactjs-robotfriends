e477931caa7247435d795047efb170aee9cb38e0
eddielewis-thinhle <eddie.thinhle@outlook.com>
2019-10-23 10:49:03 +0700

    fixing dependency-related error & minor bugs

--- src/index.js
+++ src/index.js
@@ -7,13 +7,18 @@ import App from "./App";
 // redux stuff
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "./Reducers/RootReducer.reducer";
 
 /* --------- redux store & logger -------- */
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk, logger))
+);
+
 ReactDOM.render(
   // NOTE Provider passes the store to EVERY component of the tree
   <Provider store={store}>
